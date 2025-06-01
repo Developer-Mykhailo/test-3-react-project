d7907b0ee85dc4994561c811140d5b6b0c2c26b7
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-06-01 14:20:04 -0600

    add notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Container from "./components/Container/Container";
 import Description from "./components/Description/Description";
+import Notification from "./components/Notification/Notification";
+import Options from "./components/Options/Options";
 
 function App() {
   return (
     <Container>
       <Description />
+      <Options />
+
+      <Notification />
     </Container>
   );
 }
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,13 @@
+import s from "./Options.module.css";
+
+const Options = () => {
+  return (
+    <div className={s.button_wrap}>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+    </div>
+  );
+};
+
+export default Options;
